e226eb9ec2631e97a7bf86d1d56d9d0a55362ce5
Dervisoglu08 <dervisoglutugba@gmail.com>
2023-08-15 22:23:06 +0300

    Add project files.

---
+++ Blog.Core/Entities/EntityBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public abstract class EntityBase: IEntityBase //IEntityBase'den kalıtım al dedik.
+    {
+       
+        public virtual Guid Id { get; set; }= Guid.NewGuid();
+        public virtual string CreateBy { get; set; }
+        public virtual string? ModifiedBy { get; set; }
+        public virtual string? DeletedBy { get; set; }
+        public virtual DateTime CreatedDate { get; set; }= DateTime.Now; //
+        public virtual DateTime? ModifiedDate { get; set; }
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual bool IsDeleted { get; set; }=false; //silinmemiş olarak gelecek.
+
+    }
+}
